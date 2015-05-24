438eba8fd285f870c97e7095a77874ded8e1e3a2
Carlos Agarie <carlos.agarie@gmail.com>
2015-05-24 00:04:08 -0300

    Update benchmark scripts
    Related to #6.

--- benchmark/speed.rb
+++ benchmark/speed.rb
@@ -1,20 +1,20 @@
-# Checking the speed of different integraiton methods
-
 require 'benchmark'
 require 'integration'
 
-# set the number of iterations
-iterations = 100
-puts "Benchmarking with #{iterations} iterations"
+METHODS = [:rectangle, :trapezoid, :simpson, :romberg, :adaptive_quadrature,
+           :gauss, :gauss_kronrod, :simpson3by8, :boole, :open_trapezoid,
+           :milne, :qng, :qag]
+TOL = 1e-9
+ITERATIONS = 100
 
-# put the function to be benchmarked here
-func = lambda{|x| x}
-for method in [:rectangle,:trapezoid,:simpson,:romberg, :adaptive_quadrature, :gauss, :gauss_kronrod, :simpson3by8, :boole, :open_trapezoid, :milne, :qng, :qag]
-	Benchmark.bm(25) do |bm|
-	  bm.report(method.to_s) do
-	    iterations.times do
-	        Integration.integrate(0,1,{:method=>method},&func)
-	    end
-	  end
-	end
+# Function used in the benchmark.
+f = -> x { x }
+
+puts "Benchmarking with #{ITERATIONS} iterations"
+
+Benchmark.bmbm(25) do |bm|
+  METHODS.each do |method|
+    params = { method: method, tolerance: TOL }
+    bm.report(method.to_s) { ITERATIONS.times { Integration.integrate(0, 1, params, &f) } }
+  end
 end
